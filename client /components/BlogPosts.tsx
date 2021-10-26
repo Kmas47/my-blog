0f05c3fb5fbccb98cb44c190c607d268eb119276
Krunal <kunalmasiyava@gmail.com>
2021-10-25 22:52:39 -0400

    fix: return only first 5 cards

--- client /components/BlogPosts.tsx
+++ client /components/BlogPosts.tsx
@@ -8,6 +8,7 @@ export const BlogPosts = () => {
     `BlogPosts/${GetBlogPosts}`,
     GetBlogPosts
   );
+  console.log(data);
 
   return (
     <div>
@@ -15,7 +16,7 @@ export const BlogPosts = () => {
         <h1> loading </h1>
       ) : (
         <div className="flex flex-wrap bg-gray-100 h-full">
-          {data.map((blog) => (
+          {data.slice(0, 5).map((blog) => (
             <div
               className="flex justify-center px-3 py-2 w-full md:w-1/2 xl:w-1/3 2xl:w-1/4"
               key={blog.id}
